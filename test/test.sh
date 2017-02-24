b7b8e4a03fcc939acb6cda61726678029969001d
Vishal Lal <vishal.lal@container-solutions.com>
2017-02-24 19:06:04 +0100

    Trigger buiild

--- test/test.sh
+++ test/test.sh
@@ -6,15 +6,21 @@ SCRIPT_DIR=`dirname "$0"`
 SCRIPT_NAME=`basename "$0"`
 SSH_OPTS=-oStrictHostKeyChecking=no
 
-if [[ -z $(scripts/docker.sh images | grep test-container) ]] ; then
+if [[ "$(uname)" == "Darwin" ]]; then
+    DOCKER_CMD=docker
+else
+    DOCKER_CMD="sudo docker"
+fi
+
+if [[ -z $($DOCKER_CMD images | grep test-container) ]] ; then
     echo "Building test container"
-    scripts/docker.sh build -t test-container $SCRIPT_DIR > /dev/null
+    docker build -t test-container $SCRIPT_DIR > /dev/null
 fi
 
 echo "Testing $1"
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo "$@"
-exec scripts/docker.sh run \
+$DOCKER_CMD run \
     --rm \
     --name test \
     -v /var/run/docker.sock:/var/run/docker.sock \
